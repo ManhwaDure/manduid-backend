326f1083aaa0271d0149666106b76e00088d0c79
LiteHell <litehell@litehell.info>
2021-01-27 01:41:02 +0900

    Enable graphql playground only when development

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import graphql from './graphql/server';
 import http from './http';
 
 const { GRAPHQL_PORT, HTTP_PORT } = process.env;
-graphql.start({ port: GRAPHQL_PORT, endpoint: '/' }, () =>
-  console.log(`Listening GraphQL on ${GRAPHQL_PORT}`)
+graphql.start(
+  {
+    port: GRAPHQL_PORT,
+    endpoint: '/',
+    playground:
+      process.env.NODE_ENV === 'development' ? '/' : false,
+  },
+  () => console.log(`Listening GraphQL on ${GRAPHQL_PORT}`)
 );
 http.listen(HTTP_PORT, () => {
   console.log(`Listening Http api on ${HTTP_PORT}`);
